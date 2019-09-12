a9d7d0f06e5ddc1b2c64d9a86ea9d93e35561d19
Sven Mol <s.mol@student.utwente.nl>
2019-09-12 16:48:01 +0200

    Make protocol, port and ssl validation configurable

--- src/Server.php
+++ src/Server.php
@@ -9,7 +9,10 @@ class Server
 {
 	private const ERROR_INDICATOR = 'Error: ';
 
+	private $protocol = 'https';
 	private $hostname;
+	private $port = 8083;
+	private $validate_ssl = true;
 	private $username;
 	private $password;
 
@@ -20,12 +23,36 @@ class Server
 		$this->password = $password;
 	}
 
+	/**
+	 * @param string $protocol
+	 */
+	public function setProtocol(string $protocol): void
+	{
+		$this->protocol = $protocol;
+	}
+
+	/**
+	 * @param int $port
+	 */
+	public function setPort(int $port): void
+	{
+		$this->port = $port;
+	}
+
+	/**
+	 * @param bool $validate_ssl
+	 */
+	public function validateSsl(bool $validate_ssl): void
+	{
+		$this->validate_ssl = $validate_ssl;
+	}
+
 	public function doRequest(string $command, array $data): string
 	{
-		$curl = curl_init(sprintf("https://%s:8083/api/", $this->hostname));
+		$curl = curl_init(sprintf("%s://%s:%d/api/", $this->protocol, $this->hostname, $this->port));
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, $this->validate_ssl ? 2 : 0);
 		curl_setopt($curl, CURLOPT_POST, true);
 		curl_setopt($curl, CURLOPT_POSTFIELDS, $this->generatePostData($command, $data));
 
